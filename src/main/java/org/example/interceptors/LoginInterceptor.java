54dd773366dfc1dae266517c7325935d9ba6031a
Wayne <w25hao@uwaterloo.ca>
2025-01-14 14:55:48 +0800

    initial commit - complete user register, login and JWT token generation&verification

---
+++ src/main/java/org/example/interceptors/LoginInterceptor.java
@@ -0,0 +1,4 @@
+package org.example.interceptors;
+
+public class LoginInterceptor {
+}
---
+++ src/main/java/org/example/pojo/Result.java
@@ -0,0 +1,24 @@
+package org.example.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result<T> {
+    private Integer code;
+    private String message;
+    private T data;
+
+    public static <E> Result<E> success(E data) {
+        return new Result<>(0, "Operation successful", data);
+    }
+
+    public static Result success() {
+        return new Result(0, "Operation successful", null);
+    }
+
+    public static Result error(String message) {
+        return new Result(1, message, null);
+    }
+}
